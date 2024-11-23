6db0c39c6b3d790a684ccae5ebc3ca06effae3e6
Zhadyra Shynybayeva <shzhadyra96@gmail.com>
2024-11-24 01:21:53 +0500

    added config

--- src/test/java/tests/TestBase.java
+++ src/test/java/tests/TestBase.java
@@ -2,9 +2,12 @@ package tests;
 import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.Selenide;
 import com.codeborne.selenide.logevents.SelenideLogger;
+import io.github.bonigarcia.wdm.WebDriverManager;
 import io.qameta.allure.selenide.AllureSelenide;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.remote.DesiredCapabilities;
 
 import java.util.Map;
